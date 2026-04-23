5dd3fa479170ba6dae1609e1fe7a31329b840f55
Vakaris201 <vakaris.ceida@mif.stud.vu.lt>
2026-04-23 18:38:09 +0300

    Source fileas papildomas funkcijomis

--- Studentas.cpp
+++ Studentas.cpp
@@ -4,6 +4,32 @@ Studentas::Studentas(istream& is) {
     readStudent(is);
 }
 
+double mediana(std::vector<double> paz) {
+    double mediana;
+    if (paz.empty()) {
+        return 0;
+    }
+    std::sort(paz.begin(), paz.end());
+    if (paz.size() % 2 == 0) {
+        mediana = (paz[paz.size() / 2 - 1] + paz[paz.size() / 2]) / 2.0;
+    }
+    else {
+        mediana = paz[paz.size() / 2];
+    }
+    return mediana;
+}
+
+double vidurkis(std::vector<double> paz) {
+    if (paz.empty()) {
+        return 0;
+    }
+    double sum = 0;
+    for (int j = 0; j < paz.size(); j++) {
+        sum += paz[j];
+    }
+    return sum / paz.size();
+}
+
 double Studentas::galBalas(double (*strategija)(vector<double>)) const {
     if (paz_.empty()) {
         return egzam_ * EGZAM_kof;
@@ -51,3 +77,24 @@ bool comparePagalRez(const Studentas& a, const Studentas& b) {
     return a.rez() > b.rez();
 }
 
+void rusiavimas(vector<Studentas>& A, int sort_choice) {
+    if (sort_choice == 1) {
+        sort(A.begin(), A.end(), compare);
+    }
+    else if (sort_choice == 2) {
+        sort(A.begin(), A.end(), comparePagalPavarde);
+    }
+    else if (sort_choice == 3) {
+        sort(A.begin(), A.end(), comparePagalRez);
+    }
+}
+
+void print(ostream& os, int pasirinkimas, const vector<Studentas>& A, int stud_skaicius) {
+    os << left << setw(15) << "Vardas" << setw(20) << "Pavarde";
+    (pasirinkimas == 1)? os << setw(10) << "Galutinis (Vid.)" << endl : os << setw(10) << "Galutinis (Med.)" << endl;
+
+    for (int i = 0; i < stud_skaicius; i++) {
+        os << left << setw(15) << A[i].vardas() << left << setw(20) << A[i].pavarde();
+        os << setw(10) << fixed << setprecision(2) << A[i].rez() << endl;
+    }
+}
