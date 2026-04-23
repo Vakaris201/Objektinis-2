ad5a5e155b7d2adb59a2b15d8f507ef072074a53
Vakaris201 <vakaris.ceida@mif.stud.vu.lt>
2026-04-23 18:18:16 +0300

    Sukuriamas Studentas header file'as

---
+++ Studentas.h
@@ -0,0 +1,56 @@
+#ifndef STUDENTAS_H
+#define STUDENTAS_H
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <stdexcept>
+#include <numeric>
+#include <iomanip>
+
+using std::string;
+using std::vector;
+using std::istream;
+using std::ostream;
+using std::sort;
+using std::move;
+using std::endl;
+using std::left;
+using std::setw;
+using std::fixed;
+using std::setprecision;
+
+
+
+class Studentas {
+private:
+    string vardas_;
+    string pavarde_;
+    vector<double> paz_;
+    double egzam_;
+    double rez_;
+
+public:
+    Studentas() : egzam_(0.0), rez_(0.0) {}
+    Studentas(istream& is);
+
+
+    inline string vardas() const { return vardas_; }
+    inline string pavarde() const { return pavarde_; }
+    inline double egzam() const { return egzam_; }
+    inline double rez() const { return rez_; }
+    inline const vector<double>& paz() const { return paz_; }
+    double galBalas(double (*)(vector<double>) = mediana) const;
+    void skaiciuotiRez(double (*)(vector<double>) = mediana);
+    istream& readStudent(istream& is);
+};
+
+bool compare(const Studentas& a, const Studentas& b);
+bool comparePagalPavarde(const Studentas& a, const Studentas& b); 
+bool comparePagalRez(const Studentas& a, const Studentas& b); 
+
+
+
+
+#endif 
