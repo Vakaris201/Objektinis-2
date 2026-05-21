334855590b0e00d04987e2427784202192e97a5b
Vakaris201 <vakaris.ceida@mif.stud.vu.lt>
2026-05-21 18:13:03 +0300

    Member types

---
+++ Vector.h
@@ -0,0 +1,18 @@
+template <typename T>
+class Vector {
+    public:
+    // Member types
+    using value_type = T;
+    using allocator_type = std::allocator<T>;
+    using size_type = std::size_t;
+    using difference_type = std::ptrdiff_t;
+    using reference = value_type&;
+    using const_reference = const value_type&;
+    using pointer = value_type*;
+    using const_pointer = const value_type*;
+    using iterator = value_type*;
+    using const_iterator = const value_type*;
+    using reverse_iterator = std::reverse_iterator<iterator>;
+    using const_reverse_iterator = std::reverse_iterator<const_iterator>;
+
+};
