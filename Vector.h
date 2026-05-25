ddb3a8dc4b88109a1a0b758185814b6f0745d829
Vakaris201 <vakaris.ceida@mif.stud.vu.lt>
2026-05-25 17:40:31 +0300

    Capacity, Modifiers

--- Vector.h
+++ Vector.h
@@ -141,7 +141,7 @@ class Vector {
 
     const_reference at(size_type pos) const { //Access element with bounds checking: v.at(pos)
         if(pos >= size_) {
-            throw std::out_of_range("Vector::indeksas uz ribu");
+            throw std::out_of_range("Vector::at - indeksas uz ribu");
         }
         return data_[pos];
     }
@@ -176,12 +176,28 @@ class Vector {
     const_reverse_iterator crend() const noexcept {return reverse_iterator(begin());} //Return const reverse iterator to end: v.crend()
 
     // Capacity
-    bool empty() const noexcept; //Check if vector is empty: v.empty()
-    size_type size() const noexcept; //Return number of elements: v.size()
-    size_type max_size() const noexcept; //Return maximum number of elements: v.max_size()
-    void reserve(size_type new_cap); //Request a change in capacity: v.reserve(new_cap)
-    size_type capacity() const noexcept; //Return current capacity: v.capacity()
-    void shrink_to_fit(); //Reduce capacity to fit size: v.shrink_to_fit()
+    bool empty() const noexcept {return size_ == 0;} //Check if vector is empty: v.empty()
+
+    size_type size() const noexcept {return size_;} //Return number of elements: v.size()
+
+    size_type max_size() const noexcept { //Return maximum number of elements: v.max_size()
+        return std::allocator_traits<allocator_type>::max_size(allocator_);
+    }
+
+    void reserve(size_type new_cap) { //Request a change in capacity: v.reserve(new_cap)
+        if(new_cap <= capacity_) return;
+        if(new_cap >= max_size()) {
+            throw std::length_error("Vector::reserve - per didelis dydis")
+        }
+        reallocate(new_cap);
+    }
+    size_type capacity() const noexcept {return capacity_;} //Return current capacity: v.capacity()
+
+    void shrink_to_fit() { //Reduce capacity to fit size: v.shrink_to_fit()
+        if(size_ < capacity_) {
+            reallocate(size_);
+        }
+    }
 
     // Modifiers
     void clear() noexcept { //Clear contents: v.clear()
@@ -190,24 +206,98 @@ class Vector {
         }
         size_ = 0;
     }
-    iterator insert(const_iterator pos, const T& value); //Insert element before pos: v.insert(pos, value)
-    iterator insert(const_iterator pos, T&& value); //Insert element before pos: v.insert(pos, std::move(value))
-    iterator insert(const_iterator pos, size_type count, const T& value); //Insert count copies of value before pos: v.insert(pos, count, value)
+
+    iterator insert(const_iterator pos, const T& value) {//Insert element before pos: v.insert(pos, value)
+        return emplace(pos, value);
+    }
+
+    iterator insert(const_iterator pos, T&& value) { //Insert element before pos: v.insert(pos, std::move(value))
+        return emplace(pos, std::move(value));
+    }
+
+    iterator insert(const_iterator pos, size_type count, const T& value) {//Insert count copies of value before pos: v.insert(pos, count, value)
+        if(count == 0) return const_cast<iterator>(pos);
+        size_type index = pos - data_;
+        if(size_ + count > capacity_) {
+            reallocate(std::max(capacity_ * 2, size_ + count));
+        }
+        for(size_type i = size_; i > index; i--) {
+            if(i < size_) {
+                destroy_at(data_ + i + count - 1);
+            }
+            construct_at(data_ + i + count - 1, std::move(data_[i - 1]));
+        }
+        for(size_type i = 0; i < count; i++) {
+            construct_at(data_ + index + i, value);
+        }
+        size_ += count;
+        return data_ + index;
+    }
+
     template <class InputIt>
-    iterator insert(const_iterator pos, InputIt first, InputIt last); //Insert elements from range [first, last) before pos: v.insert(pos, x.begin(), x.end())
-    iterator insert(const_iterator pos, std::initializer_list<T> list); //Insert elements from initializer list before pos: v.insert(pos, {a, b, c})
+    iterator insert(const_iterator pos, InputIt first, InputIt last) {//Insert elements from range [first, last) before pos: v.insert(pos, x.begin(), x.end())
+        size_type index = pos - data_;
+        size_type count = std::distance(first, last);
+        if(count == 0) return data_ + index;
+        if(size_ + count > capacity_) {
+            reallocate(std::max(capacity_ * 2, size_ + count));
+        }
+        for(size_type i = size_ + count - 1; i >= index + count; i--) {
+            construct_at(data_ + i, std::move(data_[i - count]));
+        }
+        size_type i = index;
+        for(auto it = first; it != last; it++) {
+            construct_at(data_ + i, *it);
+            i++;
+        }
+        size_ += count;
+        return data_ + index;
+    }
+
+    iterator insert(const_iterator pos, std::initializer_list<T> list) {//Insert elements from initializer list before pos: v.insert(pos, {a, b, c})
+        return insert(pos, list.begin(), list.end());
+    }
 
     template <class... Args>
-    iterator emplace(const_iterator pos, Args&&... args); //Construct element in-place before pos: v.emplace(pos, args...)
+    iterator emplace(const_iterator pos, Args&&... args) { //Construct element in-place before pos: v.emplace(pos, args...)
+        size_type index = pos - data_;
+        grow_if_needed();
+        for(size_type i = size_; i > index; i--) {
+            construct(data_ + i, std::move(data_[i - 1]));
+        }
+        if(index < size_) {
+            destroy_at(data_ + index);
+        }
+        construct_at(data_ + index, std::forward<Args>(args)...);
+        size++;
+        return data_ + index;
+    }
 
-    iterator erase(const_iterator pos); //Erase element at pos: v.erase(pos)
-    iterator erase(const_iterator first, const_iterator last); //Erase elements in range [first, last): v.erase(x.begin(), x.end())
+    iterator erase(const_iterator pos) {//Erase element at pos: v.erase(pos)
+        return erase(pos, pos + 1);
+    }
+
+    iterator erase(const_iterator first, const_iterator last) {//Erase elements in range [first, last): v.erase(x.begin(), x.end())
+        size_type first_index = first - data_;
+        size_type last_index = last - data_;
+        size_type count = last_index - first_index;
+        if(count == 0) return data_ + first_index;
+        for(size_type i = first_index; i < last_index; i++) {
+            destroy_at(data_ + i);
+        }
+        for(size_type i = last_index; i < size_; ++i) {
+            construct_at(data_ + i - count, std::move(data_[i]));
+        }
+        size_ -= count;
+        return data_ + first_index;
+    }
 
     void push_back(const T& value) { //Add element to end: v.push_back(value)
         grow_if_needed();
         construct_at(data_ + size_, value);
         size++;
     }
+
     void push_back(T&& value) { //Add element to end: v.push_back(std::move(value))
         grow_if_needed();
         construct_at(data_ + size_, std::move(value));
@@ -215,15 +305,32 @@ class Vector {
     }
 
     template <class... Args>
-    void emplace_back(Args&&... args); //Construct element in-place at end: v.emplace_back(args...)
+    void emplace_back(Args&&... args) {//Construct element in-place at end: v.emplace_back(args...)
+        grow_if_needed();
+        construct_at(data_ + size_, std::forward<Args>(args)...);
+        return data_[size_++];
+    }
 
-    void pop_back(); //Remove last element: v.pop_back()
+    void pop_back() {//Remove last element: v.pop_back()
+        destroy_at(data_ + size_--);
+    }
 
     void resize(size_type count) { //Change size to count: v.resize(count)
         resize(count, value_type{});
     }
-    void resize(size_type count, const value_type& value); //Change size to count and fill new elements with value: v.resize(count, value)
-
+    void resize(size_type count, const value_type& value) {//Change size to count and fill new elements with value: v.resize(count, value)
+        if(count < size_) {
+            for(size_type i = count; i < size_; ++i)
+                destroy_at(data_ + i);
+        }
+        else if(count > size_) {
+            if(count > capacity_) reallocate(count);
+            for(size_type i = size_; i < count; ++i)
+                construct_at(data_ + i, value);
+        }
+        size_ = count;
+    }
+    
     void swap(Vector& other) noexcept {  //Swap contents with another vector: v.swap(other)
         std::swap(data_, other.data_);
         std::swap(size_, other.size_);
